d7a7df052360e906c473e21ad2e90cef0fb1cdb4
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-14 14:53:37 -0300

    build - criada entidade Transferencia

---
+++ src/main/java/br/com/banco/entities/TipoTransacao.java
@@ -0,0 +1,5 @@
+package br.com.banco.entities;
+
+public enum TipoTransacao {
+    DEPOSITO, SAQUE, TRANSF_ENTRADA, TRANSF_SAIDA
+}
---
+++ src/main/java/br/com/banco/entities/Transferencia.java
@@ -0,0 +1,59 @@
+package br.com.banco.entities;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@ToString
+@Entity
+public class Transferencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "data_transferencia",
+            columnDefinition = "TIMESTAMP WITH TIME ZONE",
+            nullable = false,
+            updatable = false
+    )
+    @CreationTimestamp
+    private OffsetDateTime dataTransferencia;
+
+    @Column(precision = 20, scale = 2, nullable = false)
+    private BigDecimal valor;
+
+    @Column(length = 15, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoTransacao tipo;
+
+    @Column(name = "nome_operador_transacao", length = 50)
+    private String nomeOperadorTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id", referencedColumnName = "id_conta")
+    private Conta conta;
+
+    //#region Equals & HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transferencia that = (Transferencia) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    //#endregion
+}
