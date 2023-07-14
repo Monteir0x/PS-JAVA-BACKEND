dd8aa58b8bce663157d46434e127d8d93adc7416
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-14 14:55:47 -0300

    build - criado DTO de Transferencia

---
+++ src/main/java/br/com/banco/entities/dto/TransferenciaDto.java
@@ -0,0 +1,17 @@
+package br.com.banco.entities.dto;
+
+import br.com.banco.entities.TipoTransacao;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+public class TransferenciaDto {
+
+    private BigDecimal valor;
+    private TipoTransacao tipo;
+    private String nomeOperadorTransacao;
+    private Long contaId;
+}
