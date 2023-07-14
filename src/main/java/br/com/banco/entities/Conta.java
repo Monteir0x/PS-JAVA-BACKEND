a2732559228ac4b1fa801aa35594557172ca4669
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-14 14:45:52 -0300

    build - criada entidade Conta

---
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -0,0 +1,39 @@
+package br.com.banco.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Entity
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long id;
+
+    @Column(name = "nome_responsavel", length = 50, nullable = false)
+    private String nomeResponsavel;
+
+    //#region Equals & HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(id, conta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    //#endregion
+}
