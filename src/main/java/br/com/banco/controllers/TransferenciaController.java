f7970192e797ac698817fd8e0db3d19059bbee21
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-14 15:37:27 -0300

    feature - implementado metodo POST de Transferencia

--- src/main/java/br/com/banco/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -1,14 +1,13 @@
 package br.com.banco.controllers;
 
 import br.com.banco.entities.Transferencia;
+import br.com.banco.entities.dto.TransferenciaDto;
 import br.com.banco.services.TransferenciaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -33,5 +32,11 @@ public class TransferenciaController {
                 dataFinal
         );
         return ResponseEntity.ok(transferencias);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transferencia> save(@RequestBody TransferenciaDto transferenciaDto){
+        var transferencia = transferenciaService.save(transferenciaDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(transferencia);
     }
 }
