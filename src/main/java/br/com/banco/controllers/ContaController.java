bbaacaade74f5860d7da6c03020721db40477a13
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-14 15:07:17 -0300

    build - criado Controller de Conta com metodo de busca por ID

---
+++ src/main/java/br/com/banco/controllers/ContaController.java
@@ -0,0 +1,24 @@
+package br.com.banco.controllers;
+
+import br.com.banco.entities.Conta;
+import br.com.banco.services.ContaService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/contas")
+public class ContaController {
+    private final ContaService contaService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Conta> getConta(@PathVariable Long id) {
+        var conta = contaService.findById(id);
+        return ResponseEntity.ok(conta);
+    }
+}
